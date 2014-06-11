a2bd0ce28ccd4e6da0f7e83dad34dfba050ea153
▀█▄▌▄▐▌▌ <trahnagrom@live.co.uk>
2014-06-11 08:53:11 +0100

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,33 @@
+import requests
+
+
+a = raw_input('Enter currency to convert from?')
+a = a.upper()
+
+
+b = raw_input('Enter currency to convert from?')
+b = b.upper()
+
+
+c = float(raw_input('Enter value to convert?'))
+
+url = ('http://rate-exchange.appspot.com/currency?from=%s&to=%s&q=1') % (a, b)
+print (url)
+
+r = requests.get(url)
+print (r.json()['v'])
+
+print (c*r.json()['v'])
+
+urlalt = ('http://themoneyconverter.com/%s/%s.aspx') % (a, b)
+print (urlalt)
+
+#split and strip
+split1 = (' : 1 %s = ') % a
+strip1 = (' %s</h2>') % b
+
+ralt = requests.get(urlalt)
+d = float(ralt.text.split(split1)[1].split(strip1)[0].strip())
+print (d)
+
+print (c * d) 
