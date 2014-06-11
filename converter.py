68d840c3d936bc4fce567f9f85271852d156f0fb
▀█▄▌▄▐▌▌ <trahnagrom@live.co.uk>
2014-06-11 09:57:50 +0100

    Update converter.py

--- converter.py
+++ converter.py
@@ -9,7 +9,7 @@ b = input('Enter currency to convert from?')
 b = b.upper()
 
 
-c = float(raw_input('Enter value to convert?'))
+c = float(input('Enter value to convert?'))
 
 url = ('http://rate-exchange.appspot.com/currency?from=%s&to=%s&q=1') % (a, b)
 print (url)
